dac54f58bda21cdc0a5ddb14bd8e96e2ccef6ef7
victor.colomina <victor.colomina@dynatrace.com>
2022-07-21 16:40:21 +0200

    Complete - You can send a broadcast message to all the clients

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,11 @@ import { useEffect, useState } from "react"
 const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [newMessage, setNewMessage] = useState("")
   const [message, setMessage] = useState("");
 
   const sendMessage = () => {
-    socket.emit("send_message", { message: "hola" })
+    socket.emit("send_message", { message: newMessage })
   }
 
   useEffect(() => {
@@ -18,8 +19,19 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={() => sendMessage()}>Send message</button>
-      <span>{message}</span>
+      <br></br>
+
+      <div>
+        <span><strong>Last message:</strong> {message}</span>
+      </div>
+
+      <br></br>
+
+      <div>
+        <input onChange={(event) => setNewMessage(event.target.value)}></input>
+        <button onClick={() => sendMessage()}>Send message</button>
+      </div>
+
     </div>
   );
 }
